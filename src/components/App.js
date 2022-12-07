d1a79c7e3f1429e6ef102901f7028e7d1e3d30be
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-07 15:54:28 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+
+import "../styles/App.css";
+
 const App = () => {
-//code here
+  //code here
+  const [name, setName] = useState("____");
+  let dataN = "";
+  function store(e) {
+    dataN = e.target.value;
+  }
+  function final() {
+    setName(dataN);
+  }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id="input" onChange={(e) => store(e)}></input>
+      <button id="button" onClick={final}>
+        Click
+      </button>
+      <p id="text"> Hello my name is {name} and I study at Newton School</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
